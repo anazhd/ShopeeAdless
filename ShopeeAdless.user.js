5b6ab03a90a1558a95a3f51e6911f46fcc38f23a
Anaz Haidhar <mr.anazhd@gmail.com>
2023-09-19 15:44:00 +0800

    Update ShopeeAdless.user.js

--- ShopeeAdless.user.js
+++ ShopeeAdless.user.js
@@ -1,85 +1,40 @@
 // ==UserScript==
-// @name         Shopee No Ad List
+// @name         Shopee Ad Remover
 // @namespace    https://github.com/anazhd/ShopeeAdless/
-// @version      1.5
-// @description  Remove ad from search list.
+// @version      2.0
+// @description  Hide ads from search list.
 // @author       Anazhd
 // @match        *://shopee.com.my/*
 // @grant        none
-// @run-at       document-start
-// @require http://code.jquery.com/jquery-3.4.1.min.js
+// @run-at       document-end
 // @downloadURL https://github.com/anazhd/ShopeeAdless/raw/master/ShopeeAdless.user.js
 // @updateURL https://github.com/anazhd/ShopeeAdless/raw/master/ShopeeAdless.user.js
 // ==/UserScript==
 
-$(document).ready(function () {
+(function() {
+    'use strict';
 
-    function waitForKeyElements(
-        selectorTxt,
-        actionFunction,
-        bWaitOnce,
-        iframeSelector
-    ) {
-        var targetNodes, btargetsFound;
+    // Function to hide ads
+    function hideAds() {
+        var adElements = document.querySelectorAll('div.col-xs-2-4.shopee-search-item-result__item:has(div[data-sqe="ad"])');
+        adElements.forEach(function(element) {
+            element.style.display = 'none';
+        });
+    }
 
-        if (typeof iframeSelector == "undefined") {
-            targetNodes = jQuery(selectorTxt);
-        } else {
-            targetNodes = jQuery(iframeSelector).contents()
-                .find(selectorTxt);
-        }
+    // Wait for the page to fully load before hiding ads
+    window.addEventListener('load', function() {
+        hideAds();
+    });
 
-        if (targetNodes && targetNodes.length > 0) {
-            btargetsFound = true;
-            targetNodes.each(function () {
-                var jThis = jQuery(this);
-                var alreadyFound = jThis.data('alreadyFound') || false;
-
-                if (!alreadyFound) {
-                    var cancelFound = actionFunction(jThis);
-                    if (cancelFound)
-                        btargetsFound = false;
-                    else
-                        jThis.data('alreadyFound', true);
-                }
-            });
-        }
-        else {
-            btargetsFound = false;
-        }
-
-        var controlObj = waitForKeyElements.controlObj || {};
-        var controlKey = selectorTxt.replace(/[^\w]/g, "_");
-        var timeControl = controlObj[controlKey];
-
-        if (btargetsFound && bWaitOnce && timeControl) {
-            clearInterval(timeControl);
-            delete controlObj[controlKey]
-        }
-        else {
-            if (!timeControl) {
-                timeControl = setInterval(function () {
-                    waitForKeyElements(selectorTxt,
-                        actionFunction,
-                        bWaitOnce,
-                        iframeSelector
-                    );
-                },
-                    300
-                );
-                controlObj[controlKey] = timeControl;
+    // Watch for DOM mutations to hide ads in dynamically loaded content
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.type === 'childList') {
+                hideAds();
             }
-        }
-        waitForKeyElements.controlObj = controlObj;
-    }
+        });
+    });
 
-    waitForKeyElements(
-        ".shopee-search-item-result__item > div > a > div > div > div > div[data-sqe='ad']",
-        deleteAd
-    );
-
-    function deleteAd() {
-        document.querySelectorAll(".shopee-search-item-result__item > div > a > div > div > div > div[data-sqe='ad']").forEach(e => e.parentNode.parentNode.parentNode.remove());
-    }
-
-});
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
